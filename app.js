a93e912a0b6d71c5f0958112148037261ce5f7e4
James McTavish <jwmctavi@gmail.com>
2023-12-01 01:24:02 +0000

    We have scores and a full tileset.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const tetronimos = [
     {
-        tile: 7,
+        tile: 1,
         startOffset: -1,
         masks: [
             [0,0,0,0,
@@ -14,23 +14,105 @@ const tetronimos = [
            ]
     },
     {
-        tile: 8,
+        tile: 2,
         startOffset: -1,
         masks: [
             [0,0,0,0,
              0,0,1,0,
              1,1,1,0,
              0,0,0,0],
+            [0,1,1,0,
+             0,0,1,0,
+             0,0,1,0,
+             0,0,0,0],
+            [0,0,0,0,
+             1,1,1,0,
+             1,0,0,0,
+             0,0,0,0],
             [0,1,0,0,
              0,1,0,0,
              0,1,1,0,
              0,0,0,0],
+        ],
+    },
+    {
+        tile: 3,
+        startOffset: -1,
+        masks: [
             [0,0,0,0,
              1,0,0,0,
              1,1,1,0,
              0,0,0,0],
-            [0,1,1,0,
+            [0,0,1,0,
              0,0,1,0,
+             0,1,1,0,
+             0,0,0,0],
+            [0,0,0,0,
+             1,1,1,0,
+             0,0,1,0,
+             0,0,0,0],
+            [0,1,1,0,
+             0,1,0,0,
+             0,1,0,0,
+             0,0,0,0],
+        ],
+    },
+    {
+        tile: 4,
+        startOffset: -1,
+        masks: [
+            [0,0,0,0,
+             0,1,1,0,
+             0,1,1,0,
+             0,0,0,0],
+        ],
+    },
+    {
+        tile: 5,
+        startOffset: -1,
+        masks: [
+            [0,0,0,0,
+             0,1,0,0,
+             1,1,1,0,
+             0,0,0,0],
+            [0,1,0,0,
+             0,1,1,0,
+             0,1,0,0,
+             0,0,0,0],
+            [0,0,0,0,
+             1,1,1,0,
+             0,1,0,0,
+             0,0,0,0],
+            [0,1,0,0,
+             1,1,0,0,
+             0,1,0,0,
+             0,0,0,0],
+        ],
+    },
+    {
+        tile: 6,
+        startOffset: -1,
+        masks: [
+            [0,0,0,0,
+             1,1,0,0,
+             0,1,1,0,
+             0,0,0,0],
+            [0,0,1,0,
+             0,1,1,0,
+             0,1,0,0,
+             0,0,0,0],
+        ],
+    },
+    {
+        tile: 7,
+        startOffset: -1,
+        masks: [
+            [0,0,0,0,
+             0,1,1,0,
+             1,1,0,0,
+             0,0,0,0],
+            [0,1,0,0,
+             0,1,1,0,
              0,0,1,0,
              0,0,0,0],
         ],
@@ -52,7 +134,7 @@ class Tetris extends Phaser.Scene {
             tile: tetronimos[id].tile,
             masks: tetronimos[id].masks,
             currentMask: 0,
-            position: [5, tetronimos[id].startOffset]
+            position: [3, tetronimos[id].startOffset]
         };
         this.showTetronimo();
         if (this.isBlocked([0, 0], 0)) {
@@ -71,6 +153,41 @@ class Tetris extends Phaser.Scene {
             }
         }
 
+        this.completeLines();
+
+        this.updateWeights();
+
+        this.tetronimo = null;
+    }
+
+    updateWeights() {
+        let leftWeight = 0;
+        let rightWeight = 0;
+
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width / 2; x++) {
+                if (this.getData(x, y)) {
+                    leftWeight++;
+                }
+            }
+            for (let x = this.width / 2; x < this.width; x++) {
+                if (this.getData(x, y)) {
+                    rightWeight++;
+                }
+            }
+        }
+        this.setNumber(this.leftWeightNumbers, leftWeight);
+        this.setNumber(this.rightWeightNumbers, rightWeight);
+    }
+
+    addScore(value) {
+        this.score += value;
+        this.setNumber(this.scoreNumbers, this.score);
+    }
+
+    /** Looks for complete lines.  If found, removes them and drops the rows above down. */
+    completeLines() {
+        let lineCount = 0;
         for (let y = 0; y < this.height; y++) {
             let completeLine = true;
             for (let x = 0; x < this.width; x++) {
@@ -79,6 +196,7 @@ class Tetris extends Phaser.Scene {
                 }
             }
             if (completeLine) {
+                lineCount++;
                 for (let y2 = y; y2 > 0; y2--) {
                     for (let x = 0; x < this.width; x++) {
                         this.setData(x, y2, this.getData(x, y2-1));
@@ -89,8 +207,17 @@ class Tetris extends Phaser.Scene {
                 }
             }
         }
-
-        this.tetronimo = null;
+        if (lineCount > 0) {
+            if (lineCount == 1) {
+                this.addScore(40);
+            } else if (lineCount == 2) {
+                this.addScore(100);
+            } else if (lineCount == 3) {
+                this.addScore(300);
+            } else if (lineCount == 4) {
+                this.addScore(1200);
+            }
+        }
     }
 
     hideTetronimo() {
@@ -173,6 +300,21 @@ class Tetris extends Phaser.Scene {
         this.tiles[index].setFrame(value);
     }
 
+    setNumber(numberTiles, value) {
+        const digits = numberTiles.length;
+        let digitValue = 10 ** (digits - 1);
+        for (let i = 0; i < digits; i++) {
+            if (value < digitValue && digitValue > 1) {
+                numberTiles[i].setVisible(false);
+            } else {
+                const digit = Math.floor(value / digitValue) % 10;
+                numberTiles[i].setVisible(true);
+                numberTiles[i].setFrame(8 + digit);
+            }
+            digitValue /= 10;
+        }
+    }
+
     preload() {
         this.load.image('background', 'background.png');
         this.load.spritesheet('tiles', 'spritesheet.png', { frameWidth: 16, frameHeight: 16 });
@@ -204,10 +346,16 @@ class Tetris extends Phaser.Scene {
     create() {
         this.add.image(300, 400, 'background');
 
-        const gridX1 = 60;
-        const gridY1 = 70;
+        const gridX = 60;
+        const gridY = 70;
         const tileW = 32;
         const tileH = 32;
+        const leftWeightX = 50;
+        const rightWeightX = 250;
+        const weightY = 730;
+        const scoreX = 400;
+        const scoreY = 140;
+        const tileScale = 2;
         this.width = 10;
         this.height = 20;
 
@@ -217,10 +365,26 @@ class Tetris extends Phaser.Scene {
         for (let x = 0; x < 10; x++) {
             for (let y = 0; y < 20; y++) {
                 let index = x + y * this.width;
-                this.tiles[index] = this.add.sprite(x * tileW + gridX1, y * tileH + gridY1, 'tiles', 0);
-                this.tiles[index].scale = 2;
+                this.tiles[index] = this.add.sprite(x * tileW + gridX, y * tileH + gridY, 'tiles', 0);
+                this.tiles[index].scale = tileScale;
                 this.data[index] = 0;
             }
+        }
+
+        this.leftWeightNumbers = new Array(3);
+        this.rightWeightNumbers = new Array(3);
+        for (let i = 0; i < 3; i++) {
+            this.leftWeightNumbers[i] = this.add.sprite(i * tileW + leftWeightX, weightY, 'tiles', 0);
+            this.leftWeightNumbers[i].scale = tileScale;
+            this.rightWeightNumbers[i] = this.add.sprite(i * tileW + rightWeightX, weightY, 'tiles', 0);
+            this.rightWeightNumbers[i].scale = tileScale;
+        }
+
+        const scoreDigits = 6;
+        this.scoreNumbers = new Array(scoreDigits);
+        for (let i = 0; i < scoreDigits; i++) {
+            this.scoreNumbers[i] = this.add.sprite(i * tileW + scoreX, scoreY, 'tiles', 0);
+            this.scoreNumbers[i].scale = tileScale;
         }
 
         this.nextTick = 0;
@@ -228,15 +392,27 @@ class Tetris extends Phaser.Scene {
         this.dropRequested = false;
         this.rotateRequested = false;
         this.playing = true;
+        this.tickInterval = 500;
+        this.score = 0;
 
         this.input.keyboard.on('keydown', this.inputHandler, this);
+
+        this.updateWeights();
+        this.addScore(0);
     }
 
-    processInput(time) {
+    setNextTickTime() {
+        this.nextTick = this.currentTime + this.tickInterval;
+    }
+
+    processInput() {
         if (this.moveRequested[0] || this.moveRequested[1]) {
             if (this.tetronimo) {
                 if (!this.isBlocked(this.moveRequested, 0)) {
                     this.moveTetronimo(this.moveRequested);
+                    if (this.moveRequested[1] > 0) {
+                        this.setNextTickTime();
+                    }
                 }
             }
             this.moveRequested = [0, 0];
@@ -258,7 +434,8 @@ class Tetris extends Phaser.Scene {
                 }
                 this.moveTetronimo([0, dy-1]);
                 this.finalizeTetronimo();
-                this.nextTick = time + 500;
+                this.setNextTickTime();
+                this.addScore(dy);
             }
             this.dropRequested = false;
         }
@@ -266,7 +443,8 @@ class Tetris extends Phaser.Scene {
 
     update(time, delta) {
         if (this.playing) {
-            this.processInput(time);
+            this.currentTime = time;
+            this.processInput();
 
             if (time > this.nextTick) {
                 if (this.tetronimo) {
@@ -274,7 +452,7 @@ class Tetris extends Phaser.Scene {
                 } else {
                     this.spawnTetronimo();
                 }
-                this.nextTick = time + 500;
+                this.setNextTickTime();
             }
         }
     }
